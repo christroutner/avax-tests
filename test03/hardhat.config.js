8dd304c7f6b55de15667617cfce601b0ccddaaa6
Chris Troutner <chris.troutner@gmail.com>
2022-12-17 17:16:25 -0800

    Successfully deployed Count contract with test03

--- test03/hardhat.config.js
+++ test03/hardhat.config.js
@@ -2,5 +2,19 @@ require('@nomicfoundation/hardhat-toolbox')
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: '0.8.17'
+  solidity: '0.8.17',
+  networks: {
+    fuji: {
+      url: 'https://api.avax-test.network/ext/bc/C/rpc',
+      gasPrice: 225000000000,
+      chainId: 43113,
+      accounts: [process.env.FUJI_ACCOUNT]
+    },
+    mainnet: {
+      url: 'https://api.avax.network/ext/bc/C/rpc',
+      gasPrice: 225000000000,
+      chainId: 43114,
+      accounts: []
+    }
+  }
 }
